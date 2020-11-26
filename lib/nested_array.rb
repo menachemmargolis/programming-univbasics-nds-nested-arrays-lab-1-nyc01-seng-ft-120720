87036dea655c2536022f7a44313430bfdd810ef4
menachem m margolis <margolismendel5728@gmail.com>
2020-11-26 15:59:16 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,9 +31,9 @@ def sorted_matrix
   # However, this time, sort each internal array alphabetically by the first character
 
 
-  [ORGANIC_PRODUCE.sort,CONVENTIONAL_PRODUCE.sort]
-  return sorted_matrix
-  binding.pry
+  ORGANIC_PRODUCE.sort 
+  CONVENTIONAL_PRODUCE.sort
+
 end
 
 def matrix_lookup(matrix, row, column)
